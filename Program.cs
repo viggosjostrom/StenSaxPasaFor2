f8a80ce5b61eb7c7e6eb00df994c8f16987548f9
viggosjostrom <viggosjostrom1@gmail.com>
2023-09-28 11:17:48 +0200

    lagt till switch för poäng som räknas till vardera spelare

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using StenSaxPaseFor2;
 using System.Data;
 
+Console.WriteLine("STEN SAX PÅSE ~ Bäst av 3!");
 Players player1 = new Players();
 Players player2 = new Players();
 
@@ -25,99 +26,92 @@ Players GetRandomPlayer()
 }
 
 
-string Randomizer() // Slumpar vem som börjar
-{
-    Random r = new Random();
-    int rPlayer = r.Next(0, 2);
 
-    if (rPlayer == 0)
-    {
-        return player1.name;
-    }
-    else
-    {
-        return player2.name;
-    }
-}
+Players startingPlayer = GetRandomPlayer();
 
-string startingPlayer = Randomizer();
-Players startingPlayerNew = GetRandomPlayer();
-
-Console.WriteLine($"Välkommen {player1.name} & {player2.name}! Jag har bestämt att {startingPlayerNew.name} ska börja, helt slumpmässigt så klart.. ;)");
+Console.WriteLine($"Välkommen {player1.name} & {player2.name}! Jag har bestämt att {startingPlayer.name} ska börja, helt slumpmässigt så klart.. ;)");
 
 
-bool playerturn;
-
-if (startingPlayerNew.name == player1.name)
-{
-    playerturn = false;
-}
-else
-{
-    playerturn = true;
-}
 
 
-string player1move;
+
+
 Drag player1moveEnum;
 Drag player2moveEnum; 
 
-string player2move;
-
 player1.points = 0;
 player2.points = 0;
-int roundsCount = 3;
 bool hasWon = false;
 
 
 
-for (int i = 0; i < roundsCount; i++)
+while(!hasWon)
 {
-    if (roundsCount == 0)
+    Console.WriteLine($"{player1.name} var god välj ditt drag genom att skriva respektive siffra\n{(int)Drag.Sten}. {Drag.Sten}\n{(int)Drag.Sax}. {Drag.Sax}\n{(int)Drag.Påse}. {Drag.Påse}");
+    player1moveEnum = Enum.Parse<Drag>(Console.ReadLine());
+    Console.Clear();
+    Console.WriteLine($"{player2.name} var god välj ditt drag genom att skriva respektive siffra\n{(int)Drag.Sten}. {Drag.Sten}\n{(int)Drag.Sax}. {Drag.Sax}\n{(int)Drag.Påse}. {Drag.Påse}");
+    player2moveEnum = Enum.Parse<Drag>(Console.ReadLine());
+    Console.Clear();
+    Round(player1moveEnum, player2moveEnum);
+    if (Round(player1moveEnum, player2moveEnum) == player1.name)
     {
-        Console.WriteLine("Spelet är över. Vinnaren är: ");
-        if (player1.points > player2.points)
-        {
-            Console.Write(player1.name);
-
-        }
-        else
-        {
-            Console.Write(player2.name);
-
-        }
+        player1.points++;
+        Console.WriteLine($"{player1.name} har vunnit rundan!\nResultatet är just nu följande:\n{player1.name}: {player1.points}\n{player2.name}: {player2.points}");
+    }
+    else
+    {
+        player2.points++;
+        Console.WriteLine($"{player2.name} har vunnit rundan!\nResultatet är just nu följande:\n{player1.name}: {player1.points}\n{player2.name}: {player2.points}");
 
     }
-    if (playerturn == false)
-    {
-        Console.WriteLine($"{player1.name} var god välj ditt drag genom att skriva respektive siffra");
-        Console.WriteLine($"{(int)Drag.Sten}. {Drag.Sten}");
-        Console.WriteLine($"{(int)Drag.Sax}. {Drag.Sax}");
-        Console.WriteLine($"{(int)Drag.Påse}. {Drag.Påse}");
-        player1moveEnum = Enum.Parse<Drag>(Console.ReadLine());
-        playerturn = true;
-        Console.Clear();
-
-    }
-    if (playerturn == true)
-    {
-        Console.WriteLine($"{player2.name} var god välj ditt drag genom att skriva respektive siffra");
-        Console.WriteLine($"{(int)Drag.Sten}. {Drag.Sten}");
-        Console.WriteLine($"{(int)Drag.Sax}. {Drag.Sax}");
-        Console.WriteLine($"{(int)Drag.Påse}. {Drag.Påse}");    
-        player2moveEnum = Enum.Parse<Drag>(Console.ReadLine());
-        playerturn = false;
-        Console.Clear();
-
-
-    }
-
-
 }
 
 
 
 
 
-// if playermove 1 == playermove 2 { i--; }
-// if (player2move != "1" && player2move != "2" && player2move != "3")
+
+
+
+
+string Round(Drag player1moveEnum, Drag player2moveEnum)
+{
+    if (player2moveEnum == player1moveEnum)
+    {
+        return "Oavgjort"; 
+    }
+    switch (player1moveEnum)
+    {
+        case Drag.Sten:
+            if (player2moveEnum == Drag.Påse)
+            {
+                return player2.name;
+            }
+            else
+            {
+                return player1.name; 
+            }
+        case Drag.Sax:
+            if (player2moveEnum == Drag.Sten)
+            {
+                return player2.name;
+            }
+            else
+            {
+                return player1.name;
+            }
+        case Drag.Påse:
+            if (player2moveEnum == Drag.Sax)
+            {
+                return player2.name;
+            }
+            else
+            {
+                return player1.name;
+            }
+        default: return "error";
+     
+    }
+}
+
